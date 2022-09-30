65fdaadb5f41d7fd8bc6f10f927906e00f3a78a3
uche peter <uchehez@gmail.com>
2022-09-30 20:10:42 +0100

    initial commit

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+  {
+    productName: 'Foldable Mini Drones',
+    productNumber: '85635',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+  },
+  {
+    productName: 'Ruko fill Pro Drone',
+    productNumber: '36941',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+  },
+  {
+    productName: 'LARVENDER KF102 Drone',
+    productNumber: '36375',
+    paymentStatus: 'Refunded',
+    shipping: 'Declined'
+  },
+  {
+    productName: 'Drone with camera Drone',
+    productNumber: '68924',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+  },
+  {
+    productName: 'GPS 4k Drone',
+    productNumber: '25482',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+  },
+  {
+    productName: 'Lozenge Drone',
+    productNumber: '51635',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+  },
+  {
+    productName: 'DJI Air 2S',
+    productNumber: '00387',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+  },
+]
